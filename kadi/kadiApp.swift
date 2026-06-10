d3fff7437dd2277000a655bbb05c43bbc0e5daf9
T1collo <wachiracollins689@gmail.com>
2026-06-10 15:31:53 +0300

    Initial Commit
    Co-Authored-By: 10krhinooo <vkimanga@gmail.com>

---
+++ kadi/kadiApp.swift
@@ -0,0 +1,17 @@
+//
+//  kadiApp.swift
+//  kadi
+//
+//  Created by Collins Wachira on 10/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct kadiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
